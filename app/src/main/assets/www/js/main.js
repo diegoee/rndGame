9aa1e7164faf32b106193eaee4ddb9269d18ce77
Diego Espínola <despinolaespigares@gmail.com>
2018-04-10 00:57:44 +0200

    .

--- app/src/main/assets/www/js/main.js
+++ app/src/main/assets/www/js/main.js
@@ -1,4 +1,4 @@
-/*globals requirejs */
+/*globals requirejs, window */
 requirejs.config({
   baseUrl: 'js',
   paths: {
@@ -31,17 +31,17 @@ requirejs.config({
 requirejs([
   'jquery',
   'backbone',
-  'menu',
-  'help',
+  'text!template/main.html',
   'memoGame',
+  'snackbar',
   'bootstrap'
   ],
   function(
     $,
     Backbone,
-    menu,
-    help,
-    memoGame
+    template,
+    memoGame,
+    Snackbar
   ){
     'use strict';
 
@@ -52,11 +52,62 @@ requirejs([
           "play": "play"
       },
       init: function(){
-        menu.init(Backbone);
+        function resize(){
+          $('#body').innerHeight($(window).innerHeight());
+        }
+
+        var border = ['border-primary','border-secondary','border-success','border-danger','border-warning','border-info','border-light','border-dark'];
+        var btnColor = ['btn-primary','btn-secondary','btn-success','btn-danger','btn-warning','btn-info','btn-light'];
+
+
+        $('#container').html(template);
+        resize();
+        $(window).on('resize',resize);
+
+        var rnd = Math.floor(Math.random() * (border.length - 1)) + 1;
+        rnd = rnd-1;
+        $('#border').addClass(border[rnd]);
+
+        $.each(['#btnHelp','#btnPlay'],function(i,e){
+          rnd = Math.floor(Math.random() * (btnColor.length - 1)) + 1;
+          rnd = rnd-1;
+          $(e).addClass(btnColor[rnd]);
+        });
+
+        $('#btnHelp').on('click',function (){
+          Backbone.history.navigate('help', {trigger:true});
+        });
+        $('#btnPlay').on('click',function(){
+          Backbone.history.navigate('play', {trigger:true});
+        });
+
+        Snackbar.show({
+          text: 'App',
+          duration: 1
+        });
+
       },
       help: function() {
         this.init();
-        help.init(Backbone);
+        $('#modalHelp').modal('dispose');
+
+        var labels = ['badge-primary','badge-secondary','badge-success','badge-danger','badge-warning','badge-info','badge-light','badge-dark'];
+        var words = ['Play','Random','in the toilet','whats next!!','living la vida loca','Enjoy','Share','Live'];
+        var n = Math.floor(Math.random() * (75 - 1)) + 1;
+        var r1,r2;
+
+        for (var i=0; i<n;i++){
+          r1 = Math.floor(Math.random() * (labels.length - 1)) + 1;
+          r1 = r1-1;
+          r2 = Math.floor(Math.random() * (words.length - 1)) + 1;
+          r2 = r2-1;
+          $('#rndWords').append('<span class="badge '+labels[r1]+'">'+words[r2]+'</span>');
+        }
+
+        $('#modalHelp').modal();
+        $('#modalHelp').on('hidden.bs.modal',function () {
+          Backbone.history.navigate('init', {trigger:true});
+        });
       },
       play: function() {
         memoGame.init();
