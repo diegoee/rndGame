a080b607cf4d25772ab44b5c63a52685b6f523d0
Diego E <despinolaespigares@gmail.com>
2019-06-13 21:55:00 +0200

    .

--- app/src/main/java/com/diegoee/rndgame/MainActivity.java
+++ app/src/main/java/com/diegoee/rndgame/MainActivity.java
@@ -1,10 +1,14 @@
 package com.diegoee.rndgame;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,6 +25,10 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity{
     public static final String TAG = "TAG_LOG";
     public static final String DATA = "DATA_WEB";
@@ -34,6 +42,13 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+            checkPermission();
+        }else{
+            permissionCheck = 0;
+        }
 
         webView = createWebView();
         webView.loadUrl("file:///android_asset/www/index.html");
@@ -104,14 +119,72 @@ public class MainActivity extends AppCompatActivity{
         checkData();
     }
 
+
+
+    public static int CODE_PERM = 01;
+    int permissionCheck;
+
+
+    private void checkPermission(){
+        permissionCheck = this.checkSelfPermission("Manifest.permission.WRITE_EXTERNAL_STORAGE");
+        permissionCheck += this.checkSelfPermission("Manifest.permission.READ_EXTERNAL_STORAGE");
+        if (permissionCheck != 0) {
+            this.requestPermissions(new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            },CODE_PERM); //Any number
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if(requestCode==CODE_PERM){
+            if (grantResults.length!=0) {
+                permissionCheck = 0;
+                for (int num : grantResults) {
+                    permissionCheck = permissionCheck + num;
+                }
+            }
+        }
+    }
+
     @JavascriptInterface
     public void shareScore(int score){
-        Log.v(TAG,"shareScore "+score);
-        Intent i = new Intent( android.content.Intent.ACTION_SEND);
-        i.setType("text/plain");
-        i.putExtra(android.content.Intent.EXTRA_TEXT, "My score on RnDGame: "+score+"\nhttps://play.google.com/store/apps/details?id=com.diegoee.rndgame1");
-        startActivity(Intent.createChooser( i, "Share Via"));
-        Toast.makeText(getApplicationContext(), "Sharing!", Toast.LENGTH_SHORT).show();
+
+        try {
+
+            String mPath = Environment.getExternalStorageDirectory().toString() + "/"+getResources().getString(R.string.app_name)+".jpg";
+
+            // create bitmap screen capture
+            View v1 = getWindow().getDecorView().getRootView();
+            v1.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+            v1.setDrawingCacheEnabled(false);
+
+            File imageFile = new File(mPath);
+
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+            Log.v(TAG,"shareScore "+score);
+            Intent i = new Intent( Intent.ACTION_SEND);
+            Uri imageUri = Uri.parse(mPath);
+            i.putExtra(Intent.EXTRA_TEXT, "My score on RnDGame: "+score+"\nhttps://play.google.com/store/apps/details?id=com.diegoee.rndgame1");
+            i.putExtra(Intent.EXTRA_STREAM, imageUri);
+            i.setType("image/jpeg");
+            startActivity(Intent.createChooser( i, "Share Via"));
+            Toast.makeText(getApplicationContext(), "Sharing!", Toast.LENGTH_SHORT).show();
+
+        } catch (Throwable e) {
+            Log.v(TAG,e.getMessage());
+            Toast.makeText(getApplicationContext(), "Error, can not share :(", Toast.LENGTH_SHORT).show();
+            // Several error may come out with file handling or DOM
+            e.printStackTrace();
+        }
+
     }
 
     @JavascriptInterface
